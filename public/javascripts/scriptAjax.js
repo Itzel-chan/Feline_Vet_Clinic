4ffedf66381ea9c15e3d505e8ff889ed212399cd
Itzel-chan <cg986081@gmail.com>
2025-12-08 18:37:14 -0300

    teste

---
+++ public/javascripts/scriptAjax.js
@@ -0,0 +1,67 @@
+ $(function () {
+
+    $('.filtro').click(function () {
+
+        id = $(this).attr('id');
+
+        $.ajax({
+            url: "filtragem",
+            data: "id=" + id,
+            dataType: "json",
+            success: function (lista) {
+                cards = $(".cards");
+
+                cards.html("");
+
+                for (let index = 0; index < lista.length; index++) {
+
+                    consulta = `<div class="business-card">
+        <div class="card-left  ${lista[index].situacaoConsulta}  ">
+            <div class="logo-area">
+                <div class="logo-icon">FVC</div>
+            </div>
+        </div>
+
+        <div class="card-right">
+            <div class="name-position">
+                <h1 class="name">Consulta</h1>
+            </div>
+
+            <div class="contact-info">
+                <div class="info-item">
+                    <p>${lista[index].dataAgendada}</p>
+                    <i class="fas fa-map-marker-alt"></i>
+                </div>
+                <div class="info-item">
+                    <p>${lista[index].pet?.nome}</p>
+                    <i class="fas fa-globe"></i>
+                </div>
+                <div class="info-item">
+                    <p>${lista[index].situacaoConsulta}</p>
+                    <i class="fas fa-envelope"></i>
+                </div>
+            </div>
+
+            <div class="iconeAgenda">
+                <i class="fas fa-calendar-alt"></i>
+            </div>
+
+             <div class="btns">
+                <a href="agendar?id=${lista[index].id}">Agendar</a>
+                <a href="finalizarConsulta?id=${lista[index].id}">Finalizar</a>
+
+            </div>
+
+        </div>
+    </div>`;
+
+         cards.append(consulta);
+                }
+            }
+        })
+
+
+    })
+
+}
+)
